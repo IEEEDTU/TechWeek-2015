5d00fd4df27b11921004bd6a80ac5e9397c98d9e
sakshamsaxena <ontenth@live.com>
2015-06-28 12:07:31 +0530

    Added list

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # IEEE DTU TechWeek 2015
-
-Complete Website code of TechWeek 2015, now open source!
-
-Made by the IEEE DTU Web Team!
--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,35 @@
 /* jslint devel : true*/
 /* global $, document, window*/
-var i, j, sections = [], chapter = [];
+var i, j, sections = [], chapter = [], sectionsTitle = [];
 $(document).ready(function () {
 	'use strict';
+	sectionsTitle[0] = "Home";
+	sectionsTitle[1] = "Intro";
+	sectionsTitle[2] = "Intro 1";
+	sectionsTitle[3] = "Intro 2";
+	sectionsTitle[4] = "Intro 3";
+	sectionsTitle[5] = "Intro 4";
+	sectionsTitle[6] = "Event";
+	sectionsTitle[7] = "Event";
+	sectionsTitle[8] = "Event";
+	sectionsTitle[9] = "Event";
+	sectionsTitle[10] = "Event";
+	sectionsTitle[11] = "Event";
+	sectionsTitle[12] = "Event";
+	sectionsTitle[13] = "Event";
+	sectionsTitle[14] = "Event";
+	sectionsTitle[15] = "Event";
+	sectionsTitle[16] = "Event";
+	sectionsTitle[17] = "Event";
+	sectionsTitle[18] = "Event";
+	sectionsTitle[19] = "Event";
+	sectionsTitle[20] = "Event";
+	//Fetch all sections and make queue
 	sections = $("section.moment");//Tags array
 	for ( i = 0 ; i < sections.length ; i++ ) {
 		j = sections[i];
 		chapter[i] = parseInt($(j).data("chapter").substr(6,8));//Chapter numbers array
+		$("ul.moments-index-list").append("<li data-linkTo="+chapter[i]+"><a href='#'>"+sectionsTitle[i]+"</a></li>");
 	}
 	return chapter, sections;
 });
@@ -22,6 +45,14 @@ function getVisibleChapter () {
 $(window).load(function () {
 	$("button.trigger-backward").hide();//Hide back btn on load
 	
+	$("button.view-all").click(function () {
+		$("div.moments-index").toggleClass("show");
+	});
+	
+	$("ul.moments-index-list li").click(function () {
+		//$(this).data("linkTo")
+	});
+	
 	$("button.js-trigger").click(function () {
 		
 		var frwd = $(this).hasClass("trigger-forward");//true for forward
@@ -33,7 +64,7 @@ $(window).load(function () {
 			$("button.trigger-backward").show();
 			$($(sections[nextChapter])).fadeIn();
 			$($(sections[visibleChapter])).fadeOut();
-			if(nextChapter == 20) $("button.trigger-forward").hide();
+			if(nextChapter == (sections.length - 1)) $("button.trigger-forward").hide();
 		}
 		else {
 			$("button.trigger-forward").show();
