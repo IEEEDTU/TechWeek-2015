9375420e9268fab0d3271229c7ce72ab7d2195be
sakshamsaxena <ontenth@live.com>
2015-07-11 18:39:53 +0530

    Removed list generation

--- js/custom.js
+++ js/custom.js
@@ -3,30 +3,11 @@
 var i, j, sections = [], chapter = [], sectionsTitle = [];
 $(document).ready(function () {
 	'use strict';
-	sectionsTitle[0] = "Home";
-	sectionsTitle[1] = "Intro";
-	sectionsTitle[2] = "Intro 1";
-	sectionsTitle[3] = "Intro 2";
-	sectionsTitle[4] = "Intro 3";
-	sectionsTitle[5] = "Intro 4";
-	sectionsTitle[6] = "Event";
-	sectionsTitle[7] = "Event";
-	sectionsTitle[8] = "Event";
-	sectionsTitle[9] = "Event";
-	sectionsTitle[10] = "Event";
-	sectionsTitle[11] = "Event";
-	sectionsTitle[12] = "Event";
-	sectionsTitle[13] = "Event";
-	sectionsTitle[14] = "Event";
-	sectionsTitle[15] = "Event";
-	sectionsTitle[16] = "Event";
-	sectionsTitle[17] = "Event";
 	//Fetch all sections and make queue
 	sections = $("section.moment");//Tags array
 	for ( i = 0 ; i < sections.length ; i++ ) {
 		j = sections[i];
 		chapter[i] = parseInt($(j).data("chapter").substr(6,8));//Chapter numbers array
-		$("ul.moments-index-list").append("<li data-linkto="+chapter[i]+"><a href='#'>"+sectionsTitle[i]+"</a></li>");
 	}
 	return chapter, sections;
 });
@@ -40,30 +21,15 @@ function getVisibleChapter () {
 }
 
 $(window).load(function () {
-	$("button.trigger-backward").hide();//Hide back btn on load
+	//Hide back btn on load
+	$("button.trigger-backward").hide();
 	
+	//Toggle schedule visibility
 	$("button.view-all").click(function () {
 		$("div.moments-index").toggleClass("show");
 	});
 	
-	$("ul.moments-index-list li").click(function () {
-		$("button.trigger-backward").show();
-		var linkTo = parseInt($(this).data("linkto"));//chapter number requested
-		var sectTo = $(sections[linkTo]);
-		$("div.moments-index").toggleClass("show");
-		$(sections).not(sectTo).hide();
-		sectTo.show();
-		//Button visibility correction
-		if(linkTo === (sections.length - 1)) {
-			$("button.trigger-backward").show();
-			$("button.trigger-forward").hide();
-		}
-		if(linkTo === 0) {
-			$("button.trigger-backward").hide();
-			$("button.trigger-forward").show();
-		}
-	});
-	
+	//Forward and back btn functions
 	$("button.js-trigger").click(function () {
 		
 		var frwd = $(this).hasClass("trigger-forward");//true for forward
